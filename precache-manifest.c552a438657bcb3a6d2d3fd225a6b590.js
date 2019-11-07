1388647a74b54aa8b0d95983a83aef009df9206a
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-11-07 17:19:55 +0800

    Updates

---
+++ precache-manifest.c552a438657bcb3a6d2d3fd225a6b590.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099ce934bd73a3de6e674e18f1b343e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f609b94e669ba97c9bf3",
+    "url": "/static/css/main.860e9ab9.chunk.css"
+  },
+  {
+    "revision": "eb50538e19f99cd4d881",
+    "url": "/static/js/2.49238fa9.chunk.js"
+  },
+  {
+    "revision": "f609b94e669ba97c9bf3",
+    "url": "/static/js/main.3601c043.chunk.js"
+  },
+  {
+    "revision": "938f014dc22482255143",
+    "url": "/static/js/runtime-main.b16bc4f8.js"
+  }
+]);
